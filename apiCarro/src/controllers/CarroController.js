d18247fb46eaaa86df21d5f4f2b7b4bb297fe59d
The Leo <leonardoviana.farias@gmail.com>
2023-05-15 10:49:36 -0300

    v1

--- apiCarro/src/controllers/CarroController.js
+++ apiCarro/src/controllers/CarroController.js
@@ -1,1 +1,32 @@
 const CarroService = require('../services/CarroService');
+
+module.exports = {
+    buscarTodos: async(req,res) => {
+        let json = {error: '', result:[]};
+        
+        let carros = await CarroService.buscarTodos();
+
+        for(let i in carros){
+            json.result.push({
+                codigo: carros[i].codigo,
+                descrição: carros[i].modelo,
+                placa: carros[i].placa
+            });
+        }
+        res.json(json);
+    },
+
+    buscarUm: async(req,res) => {
+        let json = {error: '', result:{}};
+        
+        let codigo = req.params.codigo;
+        let carro = await CarroService.buscarUm(codigo);
+
+        if (carro){
+            json.result = carro;
+        }
+
+        res.json(json);
+    }
+}
+
